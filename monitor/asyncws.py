b0ff22c35fee67b0a901a853fd7cff4243adf2e7
zhangpanyi <zhangpanyi@live.com>
2018-04-28 11:20:34 +0800

    update

--- monitor/asyncws.py
+++ monitor/asyncws.py
@@ -2,7 +2,6 @@
 
 import json
 import asyncio
-import logging
 import websockets
 from . import asyncrpc
 from collections import deque
@@ -21,7 +20,6 @@ class AsyncWs(object):
         self.__callbacks = {}
         self.__request_id = 0
         self.__websocket = None
-        self.__connected = False
         self.__message_queue = deque()
         self.__num_workers = num_workers
 
@@ -36,10 +34,6 @@ class AsyncWs(object):
     async def rpc(self, params):
         """ 远程过程调用
         """
-        # 是否连接
-        if not self.__connected:
-            raise websockets.ConnectionClosed(1000, '')
-
         # 生成请求id
         self.__request_id += 1
         request_id = self.__request_id
@@ -49,13 +43,7 @@ class AsyncWs(object):
         future = self.__result[request_id] = asyncio.Future()
 
         # 异步执行请求
-        try:
-            await self.__websocket.send(json.dumps(request).encode('utf8'))
-        except Exception as e:
-            self.__websocket.close()
-            self.__connected = False
-            logging.info('Websocket write exception, %s', str(e))
-            raise e
+        await self.__websocket.send(json.dumps(request).encode('utf8'))
         await asyncio.wait_for(future, None)
         self.__result.pop(request_id)
 
@@ -99,8 +87,6 @@ class AsyncWs(object):
         """
         while True:
             if len(self.__message_queue) == 0:
-                if not self.__connected:
-                    break
                 await asyncio.sleep(0.001)
             else:
                 payload = self.__message_queue.popleft()
@@ -109,7 +95,6 @@ class AsyncWs(object):
     async def _on_open(self):
         """ 连接成功
         """
-        self.__connected = True
         await self.rpc([1, 'login', ['', '']])
         self.history_api = await self.rpc([1, 'history', []])
         self.database_api = await self.rpc([1, 'database', []])
@@ -121,33 +106,22 @@ class AsyncWs(object):
         """ 保持活跃
         """
         while True:
-            try:
-                await asyncio.sleep(10)
-                await self.rpc([0, 'get_chain_id', []])
-            except Exception as e:
-                break
+            await self.rpc([0, 'get_chain_id', []])
+            await asyncio.sleep(10)
 
     async def __handler_message(self):
         """ 消息处理
         """
-        while self.__connected:
-            payload = None
-            try:
-                payload = await self.__websocket.recv()
-                self.__message_queue.append(payload) 
-            except Exception as e:
-                self.__websocket.close()
-                self.__connected = False
-                logging.info('Websocket read exception, %s', str(e))
+        while True:
+            payload = await self.__websocket.recv()
+            self.__message_queue.append(payload)
 
     async def _handler(self):
         """ 事件循环
         """
-        while True:
-            self.__result.clear()
-            async with websockets.connect(self.url, max_size=2**20*8, max_queue=2**5*2) as websocket:
-                self.__websocket = websocket
-                task1 = asyncio.ensure_future(self._on_open())
-                task2 = asyncio.ensure_future(self.__keep_alive())
-                task3 = asyncio.ensure_future(self.__handler_message())
-                await asyncio.wait([task1, task2, task3])
+        async with websockets.connect(self.url, max_size=2**20*8, max_queue=2**5*2) as websocket:
+            self.__websocket = websocket
+            task1 = asyncio.ensure_future(self._on_open())
+            task2 = asyncio.ensure_future(self.__keep_alive())
+            task3 = asyncio.ensure_future(self.__handler_message())
+            await asyncio.wait([task1, task2, task3])

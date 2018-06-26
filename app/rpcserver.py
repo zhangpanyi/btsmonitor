6b68c4a5cc32eaaa6c43af281bbcfb7a1f099bff
zhangpanyi <zhangpanyi@live.com>
2018-06-26 21:29:14 +0800

    完成基本框架

--- app/rpcserver.py
+++ app/rpcserver.py
@@ -1,34 +1,58 @@
 # -*- coding:utf-8 -*-
 
 import asyncio
-from aiohttp.web import Application
-from aiohttp_json_rpc import JsonRpc
+from aiohttp import web
+from .asyncrpc import AsyncRPC
+from .sysconfig import SysConfig
+from jsonrpcserver.aio import methods
+
+@methods.add
+async def get_balances(context):
+    ''' 获取余额
+    '''
+    client = context['client']
+
+@methods.add
+async def get_fee(context, symbols_or_ids):
+    ''' 获取手续费
+    '''
+    client = context['client']
+
+@methods.add
+async def transfer(context, to, symbol_or_id, amount, memo):
+    ''' 资产转账
+    '''
+    client = context['client']
 
 class RpcServer(object):
     ''' json-rpc服务
     '''
 
     def __init__(self, loop=None):
-        # 初始化
         self._loop = loop
         if self._loop is None:
             self._loop = asyncio.get_event_loop()
         self._started = False
 
-        # 添加方法
-        self._rpc = JsonRpc()
-        self._rpc.add_methods(
-            ('', self.ping),
-        )
-
     def listen(self, host, port):
         ''' 监听服务
         '''
         if not self._started:
-            app = Application(loop=self._loop)
-            app.router.add_route('*', '/', self._rpc)
-            self._loop.create_server(app.make_handler(), host, port)
+            app = web.Application(loop=self._loop)
+            app.router.add_post('/', self._handle)
+            self._loop.run_until_complete(
+                self._loop.create_server(app.make_handler(), host, port))
             self._started = True
 
-    async def ping(self, request):
-        return 'pong'
+    async def _handle(self, request):
+        ''' 分发请求
+        '''
+        client = AsyncRPC(SysConfig.access, self._loop)
+        await client.wait_for_ready()
+
+        request = await request.text()
+        response = await methods.dispatch(request, context={'client': client})
+        if response.is_notification:
+            return web.Response()
+        else:
+            return web.json_response(response, status=response.http_status)
